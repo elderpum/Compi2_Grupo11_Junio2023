a5ff18c1a060240825b19a9192a0e2cb4f172856
Elder Pum <ElderPum@gmail.com>
2023-06-27 05:27:01 -0600

    CORS en front, reemplazo de localhost 6600 en front para conectar con back

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import gramatica
 from Tabla.Arbol import Arbol
 import sys
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/Compilar')
 def analysis(Contenido: str):
