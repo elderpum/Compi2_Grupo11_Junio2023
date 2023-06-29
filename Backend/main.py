2d5b4c6733a81b439eb28e4ae8b817f885c1e92f
Walter Gustavo Cotí <3064960620401@ingenieria.usac.edu.gt>
2023-06-28 18:10:38 -0600

    fixion

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request
 from flask_cors import CORS
 from flask.helpers import url_for
@@ -105,41 +104,3 @@ def salida():
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5500)
-=======
-from fastapi import FastAPI
-import gramatica
-from Tabla.Arbol import Arbol
-import sys
-import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
-
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.post('/Compilar')
-def analysis(Contenido: str):
-    try:
-        global start
-        global parse
-        sys.setrecursionlimit(100000)
-        h = gramatica.parse(Contenido)
-        ast = Arbol(h)
-        ast.ejecutar()
-        gramatica.start = ""
-        return {"consola": ast.getConsola(), "Simbolo": ast.Lista.getLista(), "Errores": ast.errores, "AST": ast.graphAST()}
-    except Exception as e:
-        return {"error": str(e)}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=6600,
-                reload=True, log_level="info", access_log=False)
->>>>>>> main
